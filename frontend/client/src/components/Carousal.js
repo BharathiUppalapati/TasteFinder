10610749bd2df919eddf5b5813598dd9202b7686
Bharathi Uppalapati <109168921+BharathiUppalapati@users.noreply.github.com>
2024-07-20 11:03:07 +0530

    Update Carousal.js

--- frontend/client/src/components/Carousal.js
+++ frontend/client/src/components/Carousal.js
@@ -10,13 +10,13 @@ export default function Carousal() {
           <div className="carousel-caption">
           </div>
           <div className="carousel-item active">
-            <img src="https://source.unsplash.com/random/900X700/?Veg staters" className="d-block w-100 carousel-img" alt="..." />
+            <img src="https://plus.unsplash.com/premium_photo-1673108852141-e8c3c22a4a22?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="d-block w-100 carousel-img" alt="..." />
           </div>
           <div className="carousel-item">
-            <img src="https://source.unsplash.com/random/900X700/?Biryani" className="d-block w-100 carousel-img" alt="..." />
+            <img src="https://images.unsplash.com/photo-1481671703460-040cb8a2d909?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="d-block w-100 carousel-img" alt="..." />
           </div>
           <div className="carousel-item">
-            <img src="https://source.unsplash.com/random/900X700/?pastry" className="d-block w-100 carousel-img" alt="..." />
+            <img src="https://images.unsplash.com/photo-1447078806655-40579c2520d6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="d-block w-100 carousel-img" alt="..." />
           </div>
         </div>
         <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
